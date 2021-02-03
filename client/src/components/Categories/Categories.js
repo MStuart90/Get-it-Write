68ca37b8af51aae22309079b8ae752cd1504e023
WD40 <69653174+duncangw1@users.noreply.github.com>
2021-02-03 13:10:14 -0500

    Created Categories component and categories.css file

--- client/src/components/Categories/Categories.js
+++ client/src/components/Categories/Categories.js
@@ -1,8 +1,58 @@
 import React from "react";
-import { Button } from 'reactstrap';
+import "./categories.css";
+import {
+  UncontrolledCollapse,
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Card,
+  CardBody,
+} from "reactstrap";
 
-
-
-
+function Categories() {
+  return (
+    <div className="p-2">
+      <Button color="info" id="toggler">
+        Categories
+      </Button>
+      <UncontrolledCollapse toggler="#toggler">
+        <Card id="categoriesCard">
+          <CardBody>
+            <Form>
+              <FormGroup tag="fieldset">
+                <FormGroup check>
+                  <Label check>
+                    <Input type="radio" name="radio1" id="category1" />{" "}
+                    Professional Emails
+                  </Label>
+                </FormGroup>
+                <FormGroup check>
+                  <Label check>
+                    <Input type="radio" name="radio1" id="category2" /> Casual
+                    Emails
+                  </Label>
+                </FormGroup>
+                <FormGroup check>
+                  <Label check>
+                    <Input type="radio" name="radio1" id="category3" />{" "}
+                    Professional Texts
+                  </Label>
+                </FormGroup>
+                <FormGroup check>
+                  <Label check>
+                    <Input type="radio" name="radio1" id="category4" /> Casual
+                    Texts
+                  </Label>
+                </FormGroup>
+              </FormGroup>
+            </Form>
+          </CardBody>
+        </Card>
+      </UncontrolledCollapse>
+    </div>
+  );
+}
 
 export default Categories;
