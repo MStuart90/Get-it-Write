a70cd6248c5e0414077b1d5de49ae0e8324262b7
Rachel Moore <moorer@gcsnc.com>
2021-02-03 09:52:53 -0500

    changed App.js to include basic layout, changed App.css to contain basic styles. installed bootstrap and react bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,30 @@
 
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Header, Container, Row, Col } from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+       <Container>
+          <header className='header'>
+            <Row>
+              <Col>Header Goes Here</Col>
+            </Row>
+          </header>
+          <article>
+            <Row className='rows'>
+              <Col className='columns'>Editable Text Area</Col>
+              <Col className='columns'>Category, and other components</Col>
+            </Row>
+          </article>
+          <footer className='footer'>
+            <Row>
+              <Col>Footer Goes Here</Col>
+            </Row>
+          </footer>
+      </Container>
       </div>
     );
   }
