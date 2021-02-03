7f36c448e6da4f7d3e115919e6f58b56759aa46e
Rachel Moore <moorer@gcsnc.com>
2021-02-03 12:35:17 -0500

    added edit text box and copy and share buttons

--- client/src/components/EditableText/EditableText.js
+++ client/src/components/EditableText/EditableText.js
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { Component } from 'react';
+import { Button, Form, FormGroup, Label, Input, FormText} from 'reactstrap';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 
 
+class EditableText extends Component {
+    render() {
+  return (
+    <Form>
+      <FormGroup>
+        <Label for="exampleText">Get it Write text:</Label>
+        <hr></hr>
+        <Input className="textArea" type="textarea" name="text" id="exampleText" />
+      </FormGroup>
 
+      {/* <CopyToClipboard text="Hello!"> */}
+      <Button variant="primary" size="sm">Copy to clipboard</Button>{' '}
+      <Button variant="primary" size="sm">Share</Button>{' '}
+      {/* </CopyToClipboard> */}
 
-export default Categories;
+    </Form>
+  );
+}
+}
+export default EditableText;
