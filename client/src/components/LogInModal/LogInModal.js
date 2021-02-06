b631dfa516b23e364bf974aff7da46b8964f7b23
Melanie Stuart <mmooradian90@yahoo.com>
2021-02-06 15:11:59 -0500

    adding modal functionality to log in

--- client/src/components/LogInModal/LogInModal.js
+++ client/src/components/LogInModal/LogInModal.js
@@ -1,5 +1,32 @@
-import React from "react";
 
+import React, { useState } from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
+const ModalExample = (props) => {
+  const {
+    buttonLabel,
+    className
+  } = props;
 
-export default Categories;
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => setModal(!modal);
+
+  return (
+    <div>
+      <Button color="danger" onClick={toggle}>{buttonLabel}</Button>
+      <Modal isOpen={modal} toggle={toggle} className={className}>
+        <ModalHeader toggle={toggle}>SIGN IN</ModalHeader>
+        <ModalBody>
+          Sign In or Sign Up
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={toggle}>Sign In</Button>{' '}
+          <Button color="secondary" onClick={toggle}>Sign Up</Button>
+        </ModalFooter>
+      </Modal>
+    </div>
+  );
+}
+
+export default ModalExample;
