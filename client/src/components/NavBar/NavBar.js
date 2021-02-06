deb33b0790eaa82ecacb90eb8027a228175840c9
Melanie Stuart <mmooradian90@yahoo.com>
2021-02-06 10:16:17 -0500

    modified navbar

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,28 +1,92 @@
+// import React, { useState } from 'react';
+// import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+
+// const Example = (props) => {
+//   const [collapsed, setCollapsed] = useState(true);
+
+//   const toggleNavbar = () => setCollapsed(!collapsed);
+
+//   return (
+//     <div>
+//       <Navbar color="faded" light>
+//         <NavbarBrand href="/" className="mr-auto">Get It Write</NavbarBrand>
+//         <NavbarToggler onClick={toggleNavbar} className="mr-2" />
+//         <Collapse isOpen={!collapsed} navbar>
+//           <Nav navbar>
+//             <NavItem>
+//               <NavLink href="/components/">Sign In</NavLink>
+//             </NavItem>
+//             <NavItem>
+//               <NavLink href="https://github.com/reactstrap/reactstrap">Sign Up</NavLink>
+//             </NavItem>
+//           </Nav>
+//         </Collapse>
+//       </Navbar>
+      
+//     </div>
+//   );
+// }
+
+// export default Example;
+
+
+
+
 import React, { useState } from 'react';
-import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText
+} from 'reactstrap';
 
 const Example = (props) => {
-  const [collapsed, setCollapsed] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
 
-  const toggleNavbar = () => setCollapsed(!collapsed);
+  const toggle = () => setIsOpen(!isOpen);
 
   return (
     <div>
-      <Navbar color="faded" light>
-        <NavbarBrand href="/" className="mr-auto">Get It Write</NavbarBrand>
-        <NavbarToggler onClick={toggleNavbar} className="mr-2" />
-        <Collapse isOpen={!collapsed} navbar>
-          <Nav navbar>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">reactstrap</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
             <NavItem>
-              <NavLink href="/components/">Sign In</NavLink>
+              <NavLink href="/components/">Components</NavLink>
             </NavItem>
             <NavItem>
-              <NavLink href="https://github.com/reactstrap/reactstrap">Sign Up</NavLink>
+              <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
             </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Options
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>
+                  Option 1
+                </DropdownItem>
+                <DropdownItem>
+                  Option 2
+                </DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>
+                  Reset
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
           </Nav>
+          <NavbarText>Simple Text</NavbarText>
         </Collapse>
       </Navbar>
-      
     </div>
   );
 }
