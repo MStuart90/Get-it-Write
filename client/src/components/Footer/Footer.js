52e7607f7a757e1d86a033ca9f89c658cc6ad4b8
Maria Byler <mariabyler98@gmail.com>
2021-02-06 13:15:51 -0500

    added avatars to footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,9 +1,135 @@
-import React from "react";
+import React, { useState } from 'react';
+import { Collapse, Button, CardBody, Card, Row, Col, UncontrolledTooltip } from 'reactstrap';
+import Gene from "../../assets/Gene.jpg"; 
+import Melanie from "../../assets/Melanie.jpg"; 
+import Rachel from "../../assets/Rachel.jpg"; 
+import Michael from "../../assets/Michael.jpg"; 
+import Maria from "../../assets/Maria.jpg"; 
 
-const Footer = () => (
-  <div className="footer">
+
+
+const Footer = (props) => {
+  const [collapse, setCollapse] = useState(false);
+  const [status, setStatus] = useState('Closed');
+
+  const onEntering = () => setStatus('Opening...');
+
+  const onEntered = () => setStatus('Opened');
+
+  const onExiting = () => setStatus('Closing...');
+
+  const onExited = () => setStatus('Closed');
+
+  const toggle = () => setCollapse(!collapse);
+
+  const imgStyle = {
+    maxHeight: 50,
+    maxWidth: 50
+  }
+
+{/* <div className="footer">
     <p>© 2021 Get it Write. All Rights Reserved</p>
-  </div>
-);
+  </div> */}
+
+  return (
+    <Row className=" mt-5">
+        <Col sm="3" xs="6">
+          <div className="avatar-group">
+            <a
+              className=" avatar avatar-xs rounded-circle"
+              style={{display: "table-cell"}} href="https://github.com/duncangw1" target="_blank"
+              id="tooltip1"
+            >
+              <img
+                className="avatar avatar-xs rounded-circle"
+                style={imgStyle}
+                alt="Gene"
+                src= {Gene}
+              ></img>
+            </a>
+            <UncontrolledTooltip delay={0} target="tooltip1">
+              Gene Duncan
+            </UncontrolledTooltip>
+            <a
+              className=" avatar avatar-xs rounded-circle profile-style"
+              style={{display: "table-cell"}} href="https://github.com/Rachelmoore2020" target="_blank"
+              id="tooltip2"
+            >
+              <img
+                className="avatar avatar-xs rounded-circle"
+                style={imgStyle}
+                alt="Rachel"
+                src= {Rachel}
+              ></img>
+            </a>
+            <UncontrolledTooltip delay={0} target="tooltip2">
+              Rachel Moore
+            </UncontrolledTooltip>
+            <a
+              className=" avatar avatar-xs rounded-circle profile-style"
+              style={{display: "table-cell"}} href="https://github.com/MStuart90" target="_blank"
+              id="tooltip3"
+            >
+              <img
+                className="avatar avatar-xs rounded-circle"
+                style={imgStyle}
+                alt="Melanie"
+                src= {Melanie}
+              ></img>
+            </a>
+            <UncontrolledTooltip delay={0} target="tooltip3">
+              Melanie Stuart
+            </UncontrolledTooltip>
+            <a
+              className=" avatar avatar-xs rounded-circle profile-style"
+              style={{display: "table-cell"}} href="https://github.com/DigitalAmnesia" target="_blank"
+              id="tooltip4"
+            >
+              <img
+                className="avatar avatar-xs rounded-circle"
+                style={imgStyle}
+                alt="Michael"
+                src= {Michael}
+              ></img>
+            </a>
+            <UncontrolledTooltip delay={0} target="tooltip4">
+              Michael Hill
+            </UncontrolledTooltip>
+            <a
+              className=" avatar avatar-xs rounded-circle profile-style"
+              style={{display: "table-cell"}} href="https://github.com/mariabyler" target="_blank"
+              id="tooltip5"
+            >
+              <img
+                className="avatar avatar-xs rounded-circle"
+                style={imgStyle}
+                alt="Maria"
+                src= {Maria}
+              ></img>
+            </a>
+            <UncontrolledTooltip delay={0} target="tooltip5">
+              Maria Byler 
+            </UncontrolledTooltip>
+          </div>
+        </Col>
+      </Row>
+    // <div>
+    //   <Button color="primary" onClick={toggle} style={{ marginBottom: '1rem' }}>Toggle</Button>
+    //   <Collapse
+    //     isOpen={collapse}
+    //     onEntering={onEntering}
+    //     onEntered={onEntered}
+    //     onExiting={onExiting}
+    //     onExited={onExited}
+    //   >
+    //     <Card>
+    //       <CardBody href="/" className="p-5"><img src={Gene} alt="Gene" id='Gene'/>
+    //       filler
+    //       </CardBody>
+    //     </Card>
+    //   </Collapse>
+    // </div>
+  );
+}
 
 export default Footer;
