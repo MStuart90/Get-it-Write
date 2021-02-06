8212e75cb004f9d4cbebdbd237eab0601c558a79
WD40 <69653174+duncangw1@users.noreply.github.com>
2021-02-06 13:32:34 -0500

    Updated App.js to use React state and pass states for the users selected categories

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,43 @@
+import React, { useState } from "react";
+import "./App.css";
+import { Row, Col } from "react-bootstrap";
+import EditableText from "./components/EditableText/EditableText";
+import NavBar from "./components/NavBar/NavBar";
+import Footer from "./components/Footer/Footer";
+import Categories from "./components/Categories/Categories";
+import Fields from "./components/Fields/Fields";
 
-import React, { Component } from 'react';
-import './App.css';
-import { Container, Row, Col } from 'react-bootstrap';
-import EditableText from './components/EditableText/EditableText';
-import NavBar from './components/NavBar/NavBar';
-import Footer from './components/Footer/Footer';
-import Categories from './components/Categories/Categories';
-
-class App extends Component {
-  render() {
-    return (
-      <div>
-       
-          <header className='header'>
-            <Row>
-              <Col className='navBar'><NavBar /></Col>
-            </Row>
-          </header>
-          <main>
-            <Row className='rows'>
-              <Col style={{paddingRight: "0px" }} className='editText'><EditableText /></Col>
-              <Col style={{paddingLeft: "0px" }} className='columns' ><Categories /></Col>
-            </Row>
-          </main>
-          <footer>
-            <Row>
-              <Col className='footer'><Footer /></Col>
-            </Row>
-          </footer>
-      
-      </div>
-    );
-  }
-}
+const App = () => {
+  const [selectedCategory, setSelectedCategory] = useState("");
+  return (
+    <div>
+      <header className="header">
+        <Row>
+          <Col className="navBar">
+            <NavBar />
+          </Col>
+        </Row>
+      </header>
+      <main>
+        <Row className="rows">
+          <Col style={{ paddingRight: "0px" }} className="editText">
+            <EditableText />
+          </Col>
+          <Col style={{ paddingLeft: "0px" }} className="columns">
+            <Categories setSelectedCategoryInApp={setSelectedCategory} />
+            <Fields selectedCategory={selectedCategory} />
+          </Col>
+        </Row>
+      </main>
+      <footer>
+        <Row>
+          <Col className="footer">
+            <Footer />
+          </Col>
+        </Row>
+      </footer>
+    </div>
+  );
+};
 
 export default App;
