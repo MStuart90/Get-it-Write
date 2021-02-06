a4703245bc4c21fdebd29501e99484edc7003df2
Michael Hill <mike@digitalamnesia.33mail.com>
2021-02-06 13:28:11 -0500

    getting a response from api/profile now in Postman.  still need to update and get rid of all the deprecation warning flying around on a the api/profile post request..

--- server/routes/api/profile.js
+++ server/routes/api/profile.js
@@ -1,22 +1,100 @@
 const express = require('express');
+const axios = require('axios');
+const config = require('config');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
+// bring in normalize to give us a proper url, regardless of what user entered
+// const normalize = require('normalize-url');
+// const checkObjectId = require('../../middleware/checkObjectId');
 
 const Profile = require('../../../models/Profile');
 const User = require('../../../models/User');
-// @route   GET api/profile/me
-// @desc    Get current users profile
-// @access  Private
+// const Post = require('../../models/Post');
+
+// @route    GET api/profile/me
+// @desc     Get current users profile
+// @access   Private
 router.get('/me', auth, async (req, res) => {
-    try {
-        const profile = await Profile.findOne({user: req.user.id}).populate('user',['name', 'avatar']);
-        if (!profile) {
-            return res.status(400).json({ msg: 'There is no profile for this user'});
-        }
-        res.json('profile');
-    } catch(err){
-        console.error(err.message);
-        res.status(500).send('Server Error');
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id
+    }).populate('user', ['name', 'avatar']);
+
+    if (!profile) {
+      return res.status(400).json({ msg: 'There is no profile for this user' });
     }
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
+
+// @route    POST api/profile
+// @desc     Create or update user profile
+// @access   Private
+router.post(
+    '/',
+    auth,
+    check('status', 'Status is required').notEmpty(),
+    check('skills', 'Skills is required').notEmpty(),
+    async (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+  
+      // destructure the request
+      const {
+        website,
+        skills,
+        youtube,
+        twitter,
+        instagram,
+        linkedin,
+        facebook,
+        // spread the rest of the fields we don't need to check
+        ...rest
+      } = req.body;
+  
+      // build a profile
+      const profileFields = {
+        user: req.user.id,
+        website:
+          website && website !== ''
+            ? normalize(website, { forceHttps: true })
+            : '',
+        skills: Array.isArray(skills)
+          ? skills
+          : skills.split(',').map((skill) => ' ' + skill.trim()),
+        ...rest
+      };
+  
+      // Build socialFields object
+      const socialFields = { youtube, twitter, instagram, linkedin, facebook };
+  
+      // normalize social fields to ensure valid url
+      for (const [key, value] of Object.entries(socialFields)) {
+        if (value && value.length > 0)
+          socialFields[key] = normalize(value, { forceHttps: true });
+      }
+      // add to profileFields
+      profileFields.social = socialFields;
+  
+      try {
+        // Using upsert option (creates new doc if no match is found):
+        let profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true, upsert: true, setDefaultsOnInsert: true }
+        );
+        return res.json(profile);
+      } catch (err) {
+        console.error(err.message);
+        return res.status(500).send('Server Error');
+      }
+    }
+  );
 module.exports = router;
