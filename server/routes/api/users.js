179f605aa7453652f9aee6a59ad48e6ae716287b
Michael Hill <mike@digitalamnesia.33mail.com>
2021-02-06 02:34:00 -0500

    added express-validator

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const {check, validationResult } = require('express-validator/check');
 
 // @route   GET api/users
 // @desc    Test route
