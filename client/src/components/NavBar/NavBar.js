d6d999d5e059e4a472612e51dde74160fcb69fa6
Melanie Stuart <mmooradian90@yahoo.com>
2021-02-07 21:52:41 -0500

    refactorednav from boot to reactstrap

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -11,3 +11,27 @@ const Example = (props) => {
 
   const toggle = () => setModal(!modal);
   // const toggleNavbar = () => setCollapsed(!collapsed);
+  return (
+    <div>
+      <Navbar color="faded" light>
+      <NavbarBrand href="/" className="mr-auto"><img src={logo} alt="Logo" id='logo' /></NavbarBrand>
+        {/* <NavbarBrand href="/" className="mr-auto">Get It Write</NavbarBrand> */}
+        {/* <NavbarToggler onClick={toggleNavbar} className="mr-2" /> */}
+        {/* <Collapse isOpen={!collapsed} navbar> */}
+          <Nav navbar>
+            <NavItem>
+              <LogInModal buttonLabel={'Log In'}/>
+            {/* <Button color="danger" onClick={toggle}>Sign In</Button>
+              <NavLink href="/components/">Sign In</NavLink> */}
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">Sign Up</NavLink>
+            </NavItem>
+          </Nav>
+        {/* </Collapse> */}
+      </Navbar>
+        </div>
+  );
+}
+
+export default Example;
