e461e39d7a6945b0ea0bcf2455204506ddbcefb9
Melanie Stuart <mmooradian90@yahoo.com>
2021-02-08 19:40:00 -0500

    input state working properly

--- client/src/components/LogInModal/LogInModal.js
+++ client/src/components/LogInModal/LogInModal.js
@@ -1,15 +1,30 @@
-import React, { useState } from 'react';
+import React, {setState, useState, state} from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+
 
 const LogInModal = (props) => {
   const {
     buttonLabel,
     className
   } = props;
-console.log('props', props) 
+ 
   const [modal, setModal] = useState(false);
 
   const toggle = () => setModal(!modal);
+  const [state, setState] = useState({
+      email: "", password: "", username: ""
+  });
+
+  const handleEmailChange = (e) => {
+      setState({email:e.target.value})
+      
+  }
+  const handlePasswordChange = (e) => {
+    setState({password:e.target.value})
+}
+const handleUsernameChange = (e) => {
+    setState({username:e.target.value})
+}
 
   return (
     <div>
@@ -18,16 +33,21 @@ console.log('props', props)
         <ModalHeader toggle={toggle}>Log In</ModalHeader>
         <ModalBody>
         <form>
+
+        <label for="username" sm={2}>username</label>
+     
+     <input type="text" onChange={handleUsernameChange} value={state.username} name="username" id="username" placeholder="username" />
+  
      
         <label for="exampleEmail" sm={2}>Email</label>
      
-          <input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+          <input type="email" onChange={handleEmailChange} value={state.email} name="email" id="exampleEmail" placeholder="with a placeholder" />
        
     
     
         <label for="examplePassword" sm={2}>Password</label>
        
-          <input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
+          <input type="password" onChange={handlePasswordChange} value={state.password} name="password" id="examplePassword" placeholder="password placeholder" />
         <input type="submit" value="Submit" />
       
       </form>
