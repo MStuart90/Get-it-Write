ba3d63ca6a5d8316666352b5893d6386b1b7d469
Melanie Stuart <mmooradian90@yahoo.com>
2021-02-08 19:14:56 -0500

    form set up in modal

--- client/src/components/LogInModal/LogInModal.js
+++ client/src/components/LogInModal/LogInModal.js
@@ -17,7 +17,20 @@ console.log('props', props)
       <Modal isOpen={modal} toggle={toggle} className={className}>
         <ModalHeader toggle={toggle}>Log In</ModalHeader>
         <ModalBody>
-          FORM- Submit user name and password  
+        <form>
+     
+        <label for="exampleEmail" sm={2}>Email</label>
+     
+          <input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+       
+    
+    
+        <label for="examplePassword" sm={2}>Password</label>
+       
+          <input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
+        <input type="submit" value="Submit" />
+      
+      </form>
         </ModalBody>
         <ModalFooter>
           <Button color="primary" onClick={toggle}>Do Something</Button>{' '}
