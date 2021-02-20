4439fb392722c8fe63c56583eda33a18048b0837
Melanie Stuart <mmooradian90@yahoo.com>
2021-02-20 12:26:45 -0500

    finalizing sign up modal

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -21,12 +21,14 @@ router.post(
     ).isLength({ min: 6 }),
   ],
   async (req, res) => {
+    console.log("failied")
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
+      
     }
 
     //deconstruct the body
