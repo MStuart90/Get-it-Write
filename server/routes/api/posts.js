3b358368af06fa51a114d22a3efb46fa725d6d2e
DigitalAmensia <mike@digitalamnesia.33mail.com>
2021-02-19 22:57:45 -0500

    added the route to get all posts...aka canned text submissions or comments

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -35,4 +35,17 @@ router.post(
   }
 );
 
+// @route   GET api/posts
+// @desc    Get all posts
+// @access  Private
+router.get("/", auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
